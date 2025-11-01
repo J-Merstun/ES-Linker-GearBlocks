76d04dca2bd78daaabdb4bd5899b2bfb50d36801
J-Merstun <junipermerstun@gmail.com>
2025-11-01 00:47:59 +0000

    auto update RPM,Torque,Brake

--- main.lua
+++ main.lua
@@ -4,8 +4,8 @@ local ESOutput = require("ESOutput")
 
 
 ----- Init UI -----
-local behaviour
-local behaviour2
+local behaviourWheel = nil
+local behaviourBrake = nil
 local SaveRPM
 local SaveTorque
 local SaveBrake
@@ -13,7 +13,7 @@ local SaveBrake
 
 local win = Windows.CreateWindow()
 win.SetAlignment( align_RightEdge, 20, 250 )
-win.SetAlignment( align_TopEdge, 80, 100 )
+win.SetAlignment( align_TopEdge, 80, 105 )
 local function onWindowClose()
 	UnloadScript.Raise( ScriptName )	-- Window closed, so unload this script.
 end
@@ -27,10 +27,10 @@ local textButton = win.CreateTextButton()
 textButton.SetAlignment( align_LeftEdge, 10, 110 )
 textButton.SetAlignment( align_TopEdge, 5, 30 )
 local function onTextButtonClicked()
-	behaviour = LocalPlayer.Value.Targeter.TargetedPart.GetBehaviour("Control Wheel")
-	SaveRPM = behaviour.GetTweakable("Max RPM")
-	SaveTorque = behaviour.GetTweakable("Peak Torque")
-	print(behaviour.Name)
+	behaviourWheel = LocalPlayer.Value.Targeter.TargetedPart.GetBehaviour("Control Wheel")
+	SaveRPM = behaviourWheel.GetTweakable("Max RPM")
+	SaveTorque = behaviourWheel.GetTweakable("Peak Torque")
+	print(behaviourWheel.Name)
 end
 textButton.OnClick.add( onTextButtonClicked )
 textButton.Text = 'Hand Wheel'
@@ -39,13 +39,11 @@ local textButton = win.CreateTextButton()
 textButton.SetAlignment( align_LeftEdge, 10, 110 )
 textButton.SetAlignment( align_BottomEdge, 5, 30 )
 local function onTextButtonClicked()
-	local ESOutput = loadfile(ScriptPath .. "ESOutput.lua")()
-	SaveRPM.Value = ESOutput.RPM
-	SaveTorque.Value = ESOutput.Torque
-	behaviour.SyncTweakables()
+	behaviourWheel = nil
+	behaviourBrake = nil
 end
 textButton.OnClick.add( onTextButtonClicked )
-textButton.Text = 'Set 100 (temp)'
+textButton.Text = 'Reset'
 
 --Brake
 
@@ -53,22 +51,22 @@ local textButton = win.CreateTextButton()
 textButton.SetAlignment( align_RightEdge, 10, 110 )
 textButton.SetAlignment( align_TopEdge, 5, 30 )
 local function onTextButtonClicked()
-	behaviour2 = LocalPlayer.Value.Targeter.TargetedPart.GetBehaviour("Brake")
-	SaveBrake = behaviour2.GetTweakable("Peak Torque")
-	print(behaviour2.Name)
+	behaviourBrake = LocalPlayer.Value.Targeter.TargetedPart.GetBehaviour("Brake")
+	SaveBrake = behaviourBrake.GetTweakable("Peak Torque")
+	print(behaviourBrake.Name)
 end
 textButton.OnClick.add( onTextButtonClicked )
 textButton.Text = 'Break'
 
-local textButton = win.CreateTextButton()
-textButton.SetAlignment( align_RightEdge, 10, 110 )
-textButton.SetAlignment( align_BottomEdge, 5, 30 )
-local function onTextButtonClicked()
-	SaveBrake.Value = ESOutput.Brake
-	behaviour2.SyncTweakables()
-end
-textButton.OnClick.add( onTextButtonClicked )
-textButton.Text = 'Set 100 (temp)'
+--local textButton = win.CreateTextButton()
+--textButton.SetAlignment( align_RightEdge, 10, 110 )
+--textButton.SetAlignment( align_BottomEdge, 5, 30 )
+--local function onTextButtonClicked()
+--	SaveBrake.Value = ESOutput.Brake
+--	behaviourBrake.SyncTweakables()
+--end
+--textButton.OnClick.add( onTextButtonClicked )
+--textButton.Text = 'Set 100 (temp)'
 
 
 
@@ -77,20 +75,37 @@ textButton.Text = 'Set 100 (temp)'
 
 function FixedUpdate()
 
---behaviour3 = 
---print(LocalPlayer.Value.Targeter.TargetedPart.GetBehaviour("Control Wheel").IsActivated)
+	--behaviour3 = 
+	--print(LocalPlayer.Value.Targeter.TargetedPart.GetBehaviour("Control Wheel").IsActivated)
 
---thing to get the names of objects
---	local localPlayer = LocalPlayer.Value
---	local targeter
---	if localPlayer then
---		targeter = localPlayer.Targeter
---	end
---	if targeter and targeter.TargetedPart then
---		for behaviour in targeter.TargetedPart.Behaviours do
---			print(behaviour.Name)
---		end
---	end
+	--thing to get the names of objects
+	--	local localPlayer = LocalPlayer.Value
+	--	local targeter
+	--	if localPlayer then
+	--		targeter = localPlayer.Targeter
+	--	end
+	--	if targeter and targeter.TargetedPart then
+	--		for behaviour in targeter.TargetedPart.Behaviours do
+	--			print(behaviour.Name)
+	--		end
+	--	end
+	if behaviourWheel ~= nil then
+		if SaveRPM.Value ~= ESOutput.RPM
+			then
+				SaveRPM.Value = ESOutput.RPM
+				SaveTorque.Value = ESOutput.Torque
+				behaviourWheel.SyncTweakables()
+		end
+	end
+
+	if behaviourBrake ~= nil then
+		if SaveBrake.Value ~= ESOutput.Brake 
+			then
+				SaveBrake.Value = ESOutput.Brake
+				behaviourBrake.SyncTweakables()
+		end
+	end
+
 
 end	
 
