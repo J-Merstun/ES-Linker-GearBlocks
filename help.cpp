d7efa41545445323d711827e34618f91ff23d3b0
J-Merstun <junipermerstun@gmail.com>
2025-11-01 14:02:16 +0000

    i fixed 2 errors, no idea abotu the last 3

--- help.cpp
+++ help.cpp
@@ -10,22 +10,23 @@ int help()
 {
   ofstream SetLua;
   string line;
+
+    int RPM = 1000;
+    int Torque = 0;
+    int Brake = 0;
+
 SetLua.open("D:/SteamLibrary/steamapps/common/GearBlocks/BepInEx/LogOutput.log");
   if (SetLua.is_open())
   {
    while (getline(SetLua, line))
     {
       cout << line << endl;
-      if (line = "[Message:     Unity] Lua: Throttle = 0") {int Torque = 0}
-      if (line = "[Message:     Unity] Lua: Throttle = 1") {int Torque = 100}
+      if (line = "[Message:     Unity] Lua: Throttle = 0") {int Torque = 0;}
+      if (line = "[Message:     Unity] Lua: Throttle = 1") {int Torque = 100;}
       std::this_thread::sleep_for(std::chrono::milliseconds(7));
     }
     SetLua.close();
   }
-
-    int RPM = 1000;
-    int Torque = 0;
-    int Brake = 0;
 
         SetLua.open("C:/Users/Owner/AppData/LocalLow/SmashHammer Games/GearBlocks/ScriptMods/ES-Linker-GearBlocks/ESOutput.lua");
         SetLua << "local ESOutput = {}\n\nESOutput.RPM = " + to_string(RPM) + "\nESOutput.Torque = " + to_string(Torque) + "\nESOutput.Brake = " + to_string(Brake) + "\nreturn ESOutput";
