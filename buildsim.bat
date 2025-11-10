e869013391db5cce84ab3835b57737f36401ef21
killgriff22 <kilgrif22033@gmail.com>
2025-11-09 19:50:30 -0500

    remove redundant build dir update buildsim.bat

--- buildsim.bat
+++ buildsim.bat
@@ -1,13 +1,12 @@
-cd "C:\Users\skye\Documents\ES-Linker-GearBlocks"
 
-git clone --recurse-submodules https://github.com/ange-yaghi/engine-sim sim-win
+git clone --recurse-submodules https://github.com/killgriff22/engine-sim sim-win
 
 cd sim-win
 rd /s /q build
 mkdir build
 cd build
 
-cmake -DCMAKE_POLICY_VERSION_MINIMUM=3.5-DBOOST_ROOT="C:/local/boost_1_89_0" -DBoost_LIBRARY_DIRS="C:/local/boost_1_89_0/lib" ..
+cmake .. -Wno-dev 
 cmake —build . -Wno-dev 
 REM engine-sim-app.exe
 cd ../../
